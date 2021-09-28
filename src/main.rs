d62a2a6b74f7fc4c7859a9a582ee12af01acf23c
Sergey Dzyuban <dzyuban@gmail.com>
2021-09-28 22:42:44 +0300

    ips

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[macro_use] extern crate rocket;
+extern crate pnet;
+
 rocket_healthz::healthz!();
+use pnet::datalink;
 
 #[get("/")]
 fn index() -> &'static str {
+    for iface in datalink::interfaces() {
+        println!("{:?}", iface.ips);
+    }
+    
     "Hello, world!"
 }
 
